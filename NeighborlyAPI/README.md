19c1e733bc418e4e24081fa1ecc6ba134a2891db
ali <alisaeedalmatrood@gmail.com>
2022-12-14 16:28:22 +0300

    added missing screenshots

---
+++ NeighborlyAPI/README.md
@@ -0,0 +1,1 @@
+# neighborlyAPI
--- NeighborlyAPI/deleteAdvertisement/__init__.py
+++ NeighborlyAPI/deleteAdvertisement/__init__.py
@@ -9,7 +9,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if id:
         try:
-            url = "mongodb://neighborlydb:WowYC4sWSLoUvu4AYoOBopOF68rTfsiPStTLXzR8t3kozKgBwwL7Zaory26QV79dx7uVPf9cggNRACDbo38K0A==@neighborlydb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@neighborlydb@"  # TODO: Update with appropriate MongoDB connection information
+            url = "mongodb://neighborlydb:4OfGUittEwpWtyu0dRkZ8upJc0JVBdXQESw3CovsOEZ8cr1gxuE8y5lPQoya3wVS1EKFAqEtkqU0ACDbBzGnvw==@neighborlydb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@neighborlydb@"  # TODO: Update with appropriate MongoDB connection information
             client = pymongo.MongoClient(url)
             database = client['neighborly']
             collection = database['advertisements']
